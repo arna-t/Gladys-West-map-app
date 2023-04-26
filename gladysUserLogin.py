8ab1162ac13a13ff45da9baefe109fcd488cfdd6
Tu D. Vu <126857769+tudvu1997@users.noreply.github.com>
2023-04-25 22:39:48 -0700

    Update User Login Module
    Updated the default Login Module with a completed module.

--- gladysUserLogin.py
+++ gladysUserLogin.py
@@ -1,21 +1,29 @@
 import io
+import re
 
 """
-	Student: Gabriel Solomon
+	Student: Tu D.Vu
 	Module: gladysUserLogin
-	Description: This module does …
+	Description: This module asks the user for their login and password. It will only accept a valid email address as login and will accept
+    any password. If the user provides an invalid login, it will print an error statement. The "while True" loop makes the function ask
+    for the user's login and password again until it receives a valid entry. In that case, it will break the loop and allow the user to 
+    access the app. 
 """
-
 
 def login():
-	"""
-		document your function definition here. what does it do?
-	"""
 
-	"""
-		delete the remaining code *in this function* and replace it with
-		your own code. add more code to do what the assignment asks of you.
-	"""
-	emailAddress = "malcomx@mecca.com"
-
-	return emailAddress
+    while True:
+        # Ask user for login and password
+        login = input("Please enter your login: ")
+        password = input("Please enter your password: ")
+        
+        # This statement ensures that the user enters a valid email address as login. 
+        # A valid email address contains 1 or more character before and after "@" 
+        # A "." after "@" followed by 1 or more character. 
+        # If the login is not a valid email, it will prompt the user to enter a valid email address again.
+        if not re.match(r"[^@]+@[^@]+\.[^@]+", login):
+            print ("ERROR: Please provide a valid email address.")
+        else:
+            # Return login if valid email and any password is provided
+            return login
+            break
