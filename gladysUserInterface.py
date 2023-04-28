b7978ffbeaa4db512e0e0605a080b742620f98cf
Arna Togayeva <arna.togayeva@gmail.com>
2023-04-27 20:21:32 -0700

    changed the input type into int

--- gladysUserInterface.py
+++ gladysUserInterface.py
@@ -22,9 +22,6 @@ def runTests():
 	average = compute.gpsAverage(4, 5)
 	print("average = ", average)
 
-	# delete the remaining code *in this function* and replace it with
-	# your code. add more code to do what the assignment asks you to do.
-	# add 3 more tests of different functions in different modules
 	print("hello!")
 
 
@@ -43,8 +40,6 @@ def runApp(userName):
 		runs the app
 	"""
 
-	#currentValue = None
-	#destinationValue = None
 	xCurrent = -1
 	yCurrent = -1
 	xDestination = -1
@@ -56,6 +51,12 @@ def runApp(userName):
 
 		print("-- Welcome to the Gladys West Map App --")
 		print("----------------------------------------")
+		print(f"user - {userName}")
+		print()
+		print(f"Current position:     x = {xCurrent}, y = {yCurrent}")
+		print(f"Destination position: x = {xDestination}, y = {yDestination}")
+		print(f"Distance: {dist}")
+		print()
 		print("[c] to set current position")
 		print("[d] to set destination position")
 		print("[m] to map, which tells the distance")
@@ -63,16 +64,13 @@ def runApp(userName):
 		print("[q] to quit")
 		print()
 
-		print(f"Current position: x = {xCurrent}, y = {yCurrent}")
-		print(f"Destination position: x = {xDestination}, y = {yDestination}")
-		print(f"Distance: {dist}")
-
 
 	# loop until user types q
 	userQuit = False
 	while (not userQuit):
 
 		#calling the menu function
+		print()
 		menu()
 
 		# get first character of input
@@ -89,53 +87,31 @@ def runApp(userName):
 			#is_valid = True
 			#while not is_valid:
 
-			xCurrent = input("Please enter a x coordinate (between 0-99): ")
-			yCurrent = input("Please enter a y coordinate (between 0-99): ")
+			xCurrent = int(input("Please enter a x coordinate (between 0-99): "))
+			yCurrent = int(input("Please enter a y coordinate (between 0-99): "))
 			if (xCurrent > 99 or xCurrent < 0) or (yCurrent > 99 or yCurrent < 0):
 				print("The values you entered are outside of range 0-99")	
 			else: 
 				current = []
-				current.append[xCurrent]
-				current.append[yCurrent]
+				current.append(xCurrent)
+				current.append(yCurrent)
 				dist = compute.distance(current, destination)
 				
-					
-
-			#currentValue = satellite.gpsValue(xCurrent, yCurrent, "altitude")  # do we need the value? only to check 
-																				# if the user input found in sat data
-			
-			#if currentValue is None: 
-			#	print("Values you entered are not found in Satelite data")
-			#else:
-				#if destinationValue is None:
-				#	print("")
-				#else:
 			
 					#print(f"distance {dist}")
 			
 		elif firstChar == 'd':
 			
-				xDestination = input("Please enter a x coordinate (between 0-99) ")
-				yDestination = input("Please enter a y coordinate (between 0-99) ")
+				xDestination = int(input("Please enter a x coordinate (between 0-99): "))
+				yDestination = int(input("Please enter a y coordinate (between 0-99): "))
 				if (xDestination > 99 or xDestination < 0) or (yDestination > 99 or yDestination < 0):
 					print("The values you entered are outside of range 0-99")	
 				else: 
 					destination = []
-					destination.append[xDestination]
-					destination.append[yDestination ]
+					destination.append(xDestination)
+					destination.append(yDestination)
 					dist = compute.distance(current, destination)
 					
-			
-			#destinationValue = satellite.gpsValue(xDestination, yDestination, "altitude")
-			# print(f"destination position: {destination}")
-			
-			# if destinationValue is None: 
-			#	print("No value found. Please enter a valid number")
-			#else:
-			#	if currentValue is None:
-			#		print("No value found. Please enter a valid number")
-			#	else:
-	
 			
 		elif firstChar == 'm':
 			dist = compute.distance(current, destination)
@@ -152,6 +128,11 @@ def runApp(userName):
 		else:
 			print("ERROR: " + firstChar + " is not a valid command")
 
+		print()
+		menu()
+		userInput = input("Enter a command: ")
+		lowerInput = userInput.lower()
+		firstChar = lowerInput[0:1]
 
 	print("\n")
 	print("Thank you for using the Gladys West Map App!")
